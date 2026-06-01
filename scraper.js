e029167919a89910a1b33e26b10a925688c1c61c
Kritika Yadav <kritika.yadav_cs24@gla.ac.in>
2026-06-01 13:11:37 +0530

    Fix SSL certificate issues

--- scraper.js
+++ scraper.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 
+const https = require("https");
 const fs = require("fs");
 const axios = require("axios");
 const cheerio = require("cheerio");
@@ -32,7 +33,13 @@ async function checkCollege(college) {
   try {
     const url = college.url;
 
-    const response = await axios.get(url);
+  
+
+    const response = await axios.get(url, {
+      httpsAgent: new https.Agent({
+        rejectUnauthorized: false,
+      }),
+    });
     const $ = cheerio.load(response.data);
 
     let savedNotices = [];
